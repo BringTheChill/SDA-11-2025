8961133e93d32ab01b2a7110f42a752871e99ff5
Robert Florin <39056172+BringTheChill@users.noreply.github.com>
2025-05-31 22:48:45 +0000

    fix solution

--- 01-Programing-language/_Power_of_two/solution.swift
+++ 01-Programing-language/_Power_of_two/solution.swift
@@ -1,9 +1,8 @@
 func isPowerOfTwo(n: Int) -> Bool {
-    return n > 0 && (n & n - 1) == 0
+    return n > 0 && (n & (n - 1)) == 0
 }
 
-func shift(_ n: Int) {
-    print(n>>1)
-}
-shift(-1)
-// print(isPowerOfTwo(n: 1))
+// Cum sa reprezinti un numar in baza doi
+// print(String(256, radix: 2))
+
+print(isPowerOfTwo(n: 128))
